de3daa332c8979f8ee01b664217b0c0cc991e669
ExMarius <144387427+ExMarius@users.noreply.github.com>
2026-04-17 03:31:16 +0300

    Update series.js

--- anime/functions/api/series.js
+++ anime/functions/api/series.js
@@ -1,1 +1,8 @@
 
+export async function onRequestGet(context) {
+    const { env } = context;
+    const series = await env.DB.prepare('SELECT * FROM series ORDER BY name').all();
+    return new Response(JSON.stringify(series.results), {
+        headers: { 'Content-Type': 'application/json' }
+    });
+}
